092f65b4375b2c2ffa808bad06ea8b0e4be2fe67
adhhamdev <msmadhhamg@gmail.com>
2024-04-06 05:49:21 +0530

    Update global CSS styles and add new components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-"use client"
+"use client";
 
+import { Poppins } from "next/font/google";
 import Button from "@/components/Button";
 import Menu from "@/components/Menu";
 import Image from "next/image";
 import { useState } from "react";
+
+const poppins = Poppins({ subsets: ["latin"], weight: "600" });
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -38,12 +41,50 @@ export default function Home() {
             CAREERS
           </a>
         </nav>
-        <Image src="/menu.svg" alt="menu" width={31} height={31} className="menu-icon mt-3" onClick={() => setIsOpen(true)} />
+        <Image
+          src="/menu.svg"
+          alt="menu"
+          width={31}
+          height={31}
+          className="menu-icon mt-3"
+          onClick={() => setIsOpen(true)}
+        />
       </header>
       <section className="hero-section bg-hero bg-cover bg-left-bottom">
         <div className="hero absolute h-700 ">
-          <h1 className=" text-5xl text-white">We Crush Your competitors, Goals, And Sales Records - Without The B.S.</h1>
-          <Button text='GET FREE CONSULTATION' />
+          <h1 className=" text-5xl text-white">
+            We Crush Your competitors, Goals, And Sales Records - Without The
+            B.S.
+          </h1>
+          <Button text="GET FREE CONSULTATION" />
+        </div>
+      </section>
+      <section className=" mx-48 my-20 flex justify-center items-center">
+        <Image
+          src="/computer.png"
+          alt="hero"
+          width={414}
+          height={414}
+          className=" mr-28"
+        />
+        <div className="service-text">
+          <h2 className={`text-2xl ${poppins.className}`}>Web & Mobile App Development</h2>
+          <p className=" text-base mt-5 mb-2">Your web and mobile Apps are pieces of the puzzle to grow your business. We use frameworks which tailor content and engagement methods to respond to different intents shown by your potential customers who interact with your business online.</p>
+          <Button text="LEARN MORE" />
+        </div>
+      </section>
+      <section className="service my-20 flex justify-center items-center">
+        <Image
+          src="/computer.png"
+          alt="hero"
+          width={414}
+          height={414}
+          className="service-image"
+        />
+        <div className="service-text">
+          <h2 className={`text-2xl ${poppins.className}`}>Web & Mobile App Development</h2>
+          <p className=" text-base mt-5 mb-2">Your web and mobile Apps are pieces of the puzzle to grow your business. We use frameworks which tailor content and engagement methods to respond to different intents shown by your potential customers who interact with your business online.</p>
+          <Button text="LEARN MORE" />
         </div>
       </section>
       <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
